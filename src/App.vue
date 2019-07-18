7524cefd43214836308d603ff1ed9fa2aa6d6914
胡俊飞 <junfeihu375@gmail.com>
2019-07-18 23:26:57 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,20 @@
 
 <script>
 //测试使用工具提交代码
+/*
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+* */
 export default {
   name: 'App'
 }
